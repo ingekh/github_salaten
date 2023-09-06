c7dae9fcc87e57a38097fd703508ee7d197bc3fe
ingekh <ingegb06.kodehode@gmail.com>
2023-09-06 14:03:37 +0200

    grid1

--- javascript, test/oppgave1.js
+++ javascript, test/oppgave1.js
@@ -1,7 +1,7 @@
 function greeter(name) {
-	const greeting = `Hei ${name}`; // "Hei" + name
+	const greeting = `Hei ${name}!`; // "Hei" + name
 	return greeting;
 }
-const userName = "John Kenneth";
+const userName = "Per Jonas";
 const greeting = greeter(userName);
 console.log(greeting);
