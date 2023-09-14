dccc5116435fa10071e36ecb5d71ee1432f8c01f
ingekh <ingegb06.kodehode@gmail.com>
2023-09-14 12:57:09 +0200

    deletefold

--- javascript, test/oppgave1.js
+++
@@ -1,7 +0,0 @@
-function greeter(name) {
-	const greeting = `Hei ${name}!`; // "Hei" + name
-	return greeting;
-}
-const userName = "Per Jonas";
-const greeting = greeter(userName);
-console.log(greeting);
