40370b79551dfe6e2f7a970a2cd9ab7010562609
ingekh <ingegb06.kodehode@gmail.com>
2023-10-23 10:29:15 +0200

    js parroting

---
+++ js_parrot_circles/index.js
@@ -0,0 +1,21 @@
+
+const inputNum = document.querySelector("#input-num");
+const circleContainer = document.querySelector("#circle-container");
+
+const makeCircles = () => {
+    if (inputNum.value < 1 || inputNum.value > 1000) {
+        window.alert("Vennligst skriv inn et tall mellom 1 og 1000");
+        return
+    }
+    const circles =document.querySelectorAll(".circle")
+    for (let circle of circles) {
+        circle.remove()
+    }
+    for (let i = 0; i < inputNum.value; i++) {
+        const circle = document.createElement("div")
+        circle.classList.add("circle")
+        circle.textContent = i + 1;
+        circle.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+        circleContainer.append(circle)
+    }
+};
