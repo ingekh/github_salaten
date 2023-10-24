7e8df9d05f4c782ecc34fa3df8580f716f6a6ad2
ingekh <ingegb06.kodehode@gmail.com>
2023-10-24 10:03:01 +0200

    aifdif

--- js_parrot_loop/index.js
+++ js_parrot_loop/index.js
@@ -1,66 +1,75 @@
 
 const fruits = [
-    "Apple",
-    "Banana",
-    "Orange",
-    "Strawberry",
-    "Grapes",
-    "Mango",
-    "Watermelon",
-    "Pineapple",
-    "Cherry",
-    "Kiwi",
-    "Pear",
-    "Blueberry",
-    "Raspberry",
-    "Lemon",
-    "Peach",
-    "Cantaloupe",
-    "Plum",
-    "Coconut",
-    "Apricot",
-    "Pomegranate",
-    "Blackberry",
-    "Lime",
-    "Avocado",
-    "Guava",
-    "Passion Fruit",
-    "Papaya",
-    "Fig",
-    "Tangerine",
-    "Cranberry",
-    "Mandarin Orange",
-    "Dragon Fruit",
-    "Lychee",
-    "Starfruit",
-    "Boysenberry",
-    "Grapefruit",
-    "Nectarine",
-    "Currant",
-    "Elderberry",
-    "Persimmon",
-    "Clementine",
-    "Rhubarb",
-    "Mulberry",
-    "Cucumber",
-    "Jackfruit",
-    "Durian",
-    "Kumquat",
-    "Loquat",
-    "Rambutan"
-  ];
-  
+  "Apple",
+  "Banana",
+  "Orange",
+  "Strawberry",
+  "Grapes",
+  "Mango",
+  "Watermelon",
+  "Pineapple",
+  "Cherry",
+  "Kiwi",
+  "Pear",
+  "Blueberry",
+  "Raspberry",
+  "Lemon",
+  "Peach",
+  "Cantaloupe",
+  "Plum",
+  "Coconut",
+  "Apricot",
+  "Pomegranate",
+  "Blackberry",
+  "Lime",
+  "Avocado",
+  "Guava",
+  "Passion Fruit",
+  "Papaya",
+  "Fig",
+  "Tangerine",
+  "Cranberry",
+  "Mandarin Orange",
+  "Dragon Fruit",
+  "Lychee",
+  "Starfruit",
+  "Boysenberry",
+  "Grapefruit",
+  "Nectarine",
+  "Currant",
+  "Elderberry",
+  "Persimmon",
+  "Clementine",
+  "Rhubarb",
+  "Mulberry",
+  "Cucumber",
+  "Jackfruit",
+  "Durian",
+  "Kumquat",
+  "Loquat",
+  "Rambutan"
+];
+
+const faceEmojis = ["😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "😊", "😇"];
+
+const fruitContainer = document.querySelector("#fruit-container");
 const fruitList = document.createElement("ul");
 
 /*
 <ul>
-    <li>Banana</li>
+	<li>Banana</li>
+	<li>Banana</li>
+	<li>Banana</li>
+	<li>Banana</li>
+	<li>Banana</li>
+	<li>Banana</li>
 </ul>
 */
-for (let fruit of fruits) {
-const fruitItem = document.createElement("li");
-fruitItem.textContent = fruit;
-fruitList.append(fruitItem);
+for (let i = 0; i < fruits.length; i++) {
+  const fruitItem = document.createElement("li");
+  const randomEmoji = faceEmojis[Math.floor(Math.random() * faceEmojis.length)];
+  fruitItem.textContent = `${fruits[i]}${randomEmoji}`;
+  fruitList.append(fruitItem);
 }
 
-document.body.append(fruitList);
+fruitContainer.append(fruitList);
